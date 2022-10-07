220cce1b144b19d6627c22e9dd004d9e0a9bb63e
wodydtns <wodydtns@naver.com>
2022-10-07 12:29:32 +0900

    fix : secretKey 소거

--- wanted_pre_onboarding/settings.py
+++ wanted_pre_onboarding/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e(e$6$_w@2+i2v)inf)zwvr*8k=26e8%gh5d$ck@5nbao0=*(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
